0dc9fdfae09d37a290bc178ed4b04a3a7bc6936b
caliz83 <lizg1308@gmail.com>
2022-10-19 20:08:45 -0700

    add everything to project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Elizabeth Garcia
+//8/6/2022
+//      Mini Challenge 1: Say Hello
+//Create a brand new console project where you are asked your name. YOu will then print out the following: "Hello <name entered>." )Name entered should be 
+//the variable the user types in. 
+//Reviewed by: Michelle
+
+string yourName;
+
+Console.WriteLine("What is your name?");
+yourName = Console.ReadLine();
+Console.WriteLine("Hello, " + yourName +".");
